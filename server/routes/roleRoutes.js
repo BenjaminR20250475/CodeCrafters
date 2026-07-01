e668c6fee96d119f4f4e0f5b7f35d0b0a5ce5e0c
Ben <20250475@mywhitecliffe.com>
2026-07-01 19:45:28 +1200

    Added JWT authentication and role based route access

--- server/routes/roleRoutes.js
+++ server/routes/roleRoutes.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const roleController = require("../controllers/roleController");
+const role = require("../middleware/requireRole");
+const auth = require("../middleware/tokenAuth");
 
-router.post("/", roleController.createRole);
-router.delete("/:id", roleController.deleteRole);
+router.post("/", auth, role(3), roleController.createRole);
+router.delete("/:id", auth, role(3), roleController.deleteRole);
 
 module.exports = router;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controllers/userController");
+const authController = require("../controllers/authController");
+const role = require("../middleware/requireRole");
+const auth = require("../middleware/tokenAuth");
 
-router.post("/", userController.createUser);
-router.delete("/:id", userController.deleteUser);
+router.post("/", auth, role(3), userController.createUser);
+router.delete("/:id", auth, role(3), userController.deleteUser);
+router.post("/login", authController.login);
 
 module.exports = router;
