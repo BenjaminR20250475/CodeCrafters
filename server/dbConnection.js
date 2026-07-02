c9df46345a11c8f9231708b3e3389a3e0d5a4eeb
Ben <20250475@mywhitecliffe.com>
2026-07-02 17:29:06 +1200

    Cleanup

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,10 +1,5 @@
 const mysql = require("mysql2");
 
-console.log("host =", process.env.host);
-console.log("user =", process.env.user);
-console.log("password =", process.env.password);
-console.log("database =", process.env.database);
-
 const pool = mysql.createPool({
   host: process.env.host,
   user: process.env.user,
